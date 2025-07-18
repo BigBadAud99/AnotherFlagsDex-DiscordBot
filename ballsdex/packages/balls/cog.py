7ce5179dc1a78f5a1fd2d5e9db1ab72f42ae8e77
BigBadAud99 <100069603+BigBadAud99@users.noreply.github.com>
2025-07-18 10:47:00 +0700

    Update cog.py

--- ballsdex/packages/balls/cog.py
+++ ballsdex/packages/balls/cog.py
@@ -18,7 +18,6 @@ from ballsdex.core.models import (
     Trade,
     TradeObject,
     balls,
-    Ball,
 )
 from ballsdex.core.utils.buttons import ConfirmChoiceView
 from ballsdex.core.utils.paginator import FieldPageSource, Pages
@@ -1042,35 +1041,4 @@ class Balls(commands.GroupCog, group_name=settings.players_group_cog_name):
         
         pages = Pages(source=source, interaction=interaction)
         await pages.start(ephemeral=True)
-
-    @app_commands.command()
-    @app_commands.checks.cooldown(1, 5, key=lambda i: i.user.id)
-    async def inspect(self, interaction: discord.Interaction, ball: BallEnabledTransform):
-        """
-        Display info from a countryball without having the countryball.
-
-        Parameters
-        ----------
-        ball: BallInstance
-            The countryball you want to inspect
-        """
-        emoji = (
-            self.bot.get_emoji(ball.emoji_id) or ""
-        )  # Get emoji or an empty string if not found
-        embed = discord.Embed(
-            title=f"{emoji} {ball.country} Information:",
-            description=(
-                f"**⋄ Short Name:** {ball.short_name}\n"
-                f"**⋄ Catch Names:** {''.join(ball.catch_names)}\n"
-                f"**⋄ Rarity:** {ball.rarity}\n"
-                f"**⋄ HP:** {ball.health}\n"
-                f"**⋄ ATK:** {ball.attack}\n"
-                f"**⋄ Capacity Name:** {ball.capacity_name}\n"
-                f"**⋄ Capacity Description:** {ball.capacity_description}\n"
-                f"**⋄ Credits:** {ball.credits}\n"
-            ),
-            color=discord.Colour.blurple()
-        )
-        await interaction.response.send_message(
-            embed=embed
-        )  # Send the ball information embed as a response
+        
